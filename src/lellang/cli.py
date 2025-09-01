e601705d6f0f6b3e7a16bcbe61fa5ad8c4e701e3
CheetahDoesStuff <bravestcheetah@gmail.com>
2025-09-01 19:44:32 +0200

    whitespace and log save path logging

--- src/lellang/cli.py
+++ src/lellang/cli.py
@@ -13,15 +13,21 @@ def handle_args():
 
 
 def edit_settings(args) -> None:
-    g.file_path = args.file
+
+    g.log.info(f"Logs are saved at {g.log_dir} - Logs are not automatically cleared.")
+
+    if args.file.endswith(".lel"):
+        g.file_path = args.file
+    else:
+        g.log.error("[Argument Handler] Invalid Input file: Doesnt end with .lel")
+        g.exit()
+
 
     if args.output and args.output.endswith(".bean"):
         g.output_path = args.output
-
     elif args.output:
-        g.log.error(f"[Argument Handler] Invalid Output file: Doesnt end with .bean")
+        g.log.error("[Argument Handler] Invalid Output file: Doesnt end with .bean")
         g.exit()
-
     else:
         g.output_path = args.file[:-4] + ".bean"
 
