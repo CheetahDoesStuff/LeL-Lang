3cb8d415252a76e8d76d183e29a5fc9313f7d22e
CheetahDoesStuff <bravestcheetah@gmail.com>
2025-09-01 19:14:42 +0200

    command line command

--- src/lellang/cli.py
+++ src/lellang/cli.py
@@ -1,14 +1,18 @@
-from lellang.globals import globals
+import lellang.globals as g
 from lellang.compiler.compiler import Compiler
 import argparse
 
 
-def handle_args() -> list:
-    return []
+def handle_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("file", help="The Path To The LeL File")
+
+    return parser.parse_args()
 
 
-def edit_settings(args: list) -> None:
-    pass
+def edit_settings(args) -> None:
+    g.file_path = args.file
 
 
 def main():
--- src/lellang/globals.py
+++ src/lellang/globals.py
@@ -1,11 +1,11 @@
 from usefullog import logger
 from platformdirs import user_log_dir
 
-class __globals:
-    def __init__(self):
+log = logger.Logger(
+    "LeLlang",
+    do_timestamps=False,
+    do_log_saving=True,
+    log_save_folder=user_log_dir("LeLlang")
+)
 
-        self.log = logger.Logger("LeLlang", do_timestamps=False, do_log_saving=True, log_save_folder=user_log_dir("LeLlang"))
-
-
-
-globals = __globals
+file_path: str = ""
