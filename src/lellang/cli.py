3b5e1eb5c9cda766a9925fc5aa47492f0561670c
CheetahDoesStuff <bravestcheetah@gmail.com>
2025-09-01 18:55:52 +0200

    made log accessible to any script

--- src/lellang/cli.py
+++ src/lellang/cli.py
@@ -1,1 +1,2 @@
 from lellang.globals import globals
+
--- src/lellang/globals.py
+++ src/lellang/globals.py
@@ -4,7 +4,7 @@ from platformdirs import user_log_dir
 class __globals:
     def __init__(self):
 
-        log = logger.Logger("LeLlang", do_timestamps=False, do_log_saving=True, log_save_folder=user_log_dir("LeLlang"))
+        self.log = logger.Logger("LeLlang", do_timestamps=False, do_log_saving=True, log_save_folder=user_log_dir("LeLlang"))
 
 
 
