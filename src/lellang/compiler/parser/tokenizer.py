3a40949e96e51f60a2325064d2b2b288032a8a8c
CheetahDoesStuff <bravestcheetah@gmail.com>
2025-09-06 15:35:55 +0200

    tokenizer loop

--- src/lellang/compiler/parser/tokenizer.py
+++ src/lellang/compiler/parser/tokenizer.py
@@ -2,12 +2,18 @@ from lellang.globals import log
 
 class Tokenizer:
 
-    def __init__(self, processed) -> None:
+    def __init__(self, processed: list) -> None:
         self.processed =processed
     
     def tokenize(self) -> dict:
 
         tokens = {}
         line_index = 0
+
+        for i in range(len(self.processed)):
+
+            line = self.processed[line_index]
+
+            
 
         return tokens
