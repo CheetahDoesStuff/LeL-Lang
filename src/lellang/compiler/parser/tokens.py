7adb3a2a1ab15320184866541d4a537982c509b4
CheetahDoesStuff <bravestcheetah@gmail.com>
2025-09-06 15:40:42 +0200

    more tokens

--- src/lellang/compiler/parser/tokens.py
+++ src/lellang/compiler/parser/tokens.py
@@ -2,13 +2,22 @@ from types import SimpleNamespace
 
 TOKENS = SimpleNamespace(
 
-    var_dec = "var",
-    var_name = "rmv",
+    var_dec = "VAR_DECLARATION",
+    var_name = "VAR_NAME",
 
-    equal = "=",
-
+    var_rm = "VAR_REMOVE",
+    var_upd = "VAR_UPDATE",
     val = "VALUE",
 
+    func_dec = "FUNCTION_DECLARATION",
+
+    equal = "EQUAL",
+
     eol = "END_OF_LINE",
+
+    open_func = "OPEN_FUNCTION_BRACET",
+    close_func = "CLOSE_FUNCTION_BRACET",
+    open_calc = "OPEN_CALCULATION_PARANTHESIS",
+    close_calc = "CLOSE_CALCULATION_PARANTHESIS",
 
 )
